4ee0f760c171914602485bc5ddffdbce7eb328be
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-02-09 23:04:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
 # your code goes here
+
+def begins_with_r(array)
+ i = 0
+  if i < array.length
+  array.all? do |x|
+    x.start_with? "r"
+  end
+else
+  false
+end
+end
+
+def contain_a(array)
+  array.find_all do |x|
+    x.include? "a"
+end
+end
+
+def first_wa(array)
+ array.select do |x|
+    x.include? "wa"
+end
+end
+
+def remove_non_strings(array)
+  #array.delete_if do |x|
+    
+end
